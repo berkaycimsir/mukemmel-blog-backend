3a829a6e8b22003b8ec6175e3f9dde642ad18272
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-13 23:57:16 +0300

    get user by id and get all users query completed

--- backend/src/graphql/resolvers/query/Query.ts
+++ backend/src/graphql/resolvers/query/Query.ts
@@ -1,3 +1,23 @@
-export const Query = {
-  hello: () => "Hello"
+import { IQueryType } from "../../../@types/ResolverTypes";
+import { IUserResolverReturnType } from "../../../@types/ReturnTypes";
+import User, { IUser } from "../../../models/User";
+
+export const Query: IQueryType = {
+  // user queries
+  user: async (_, { id }): Promise<IUserResolverReturnType> => {
+    const user: IUser = await User.findById(id);
+
+    if (!user) {
+      return {
+        user: null,
+        errorMessage: "User does not exists."
+      };
+    }
+
+    return {
+      user,
+      errorMessage: "No error."
+    };
+  },
+  users: async (): Promise<IUser[]> => await User.find({})
 };
