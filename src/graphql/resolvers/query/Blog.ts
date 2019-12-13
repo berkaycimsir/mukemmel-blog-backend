d73e975ad3ca345f5fb5065449b2b22b9bd75594
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-14 00:58:39 +0300

    get comments of the blog post query

--- src/graphql/resolvers/query/Blog.ts
+++ src/graphql/resolvers/query/Blog.ts
@@ -1,8 +1,12 @@
 import { IQueryType } from "../../../@types/ResolverTypes";
 import User, { IUser } from "../../../models/User";
+import Comment, { IComment } from "../../../models/Comment";
 
 export const Blog: IQueryType = {
   user: async (parent): Promise<IUser> => {
     return await User.findById(parent.owner_id);
+  },
+  comments: async (parent): Promise<IComment[]> => {
+    return await Comment.find({ blog_id: parent.id });
   }
 };
