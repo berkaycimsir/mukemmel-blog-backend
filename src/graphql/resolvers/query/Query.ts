3809238a27fbfc26abf4836322343de00b951b0e
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2019-12-14 00:24:13 +0300

    get blog by id and get all blogs query has defined

--- src/graphql/resolvers/query/Query.ts
+++ src/graphql/resolvers/query/Query.ts
@@ -1,9 +1,12 @@
 import { IQueryType } from "../../../@types/ResolverTypes";
-import { IUserResolverReturnType } from "../../../@types/ReturnTypes";
+import {
+  IUserResolverReturnType,
+  IBlogResolverReturnType
+} from "../../../@types/ReturnTypes";
 import User, { IUser } from "../../../models/User";
+import Blog, { IBlog } from "../../../models/Blog";
 
 export const Query: IQueryType = {
-  hello: () => "Hello",
   // user queries
   user: async (_, { id }): Promise<IUserResolverReturnType> => {
     const user: IUser = await User.findById(id);
@@ -20,5 +23,28 @@ export const Query: IQueryType = {
       errorMessage: "No error."
     };
   },
-  users: async (): Promise<IUser[]> => await User.find({})
+  users: async (): Promise<IUser[]> => await User.find({}),
+
+  // blog queries
+  blog: async (_, { id }): Promise<IBlogResolverReturnType> => {
+    const blog: IBlog = await Blog.findById(id);
+
+    if (!blog) {
+      return {
+        blog: null,
+        errorMessage: "Blog does not exists."
+      };
+    }
+
+    return {
+      blog,
+      errorMessage: "No error."
+    };
+  },
+  blogs: async (): Promise<IBlog[]> => {
+    const blogs = await Blog.find({});
+    return blogs.sort(
+      (a, b) => Number(a.createdAt) - Number(b.createdAt) > 0 && -1
+    );
+  }
 };
