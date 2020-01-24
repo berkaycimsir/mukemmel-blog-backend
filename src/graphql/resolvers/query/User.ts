dafdd253f93aabd5617e67add00a9c91926fa1fb
berkaycimsir <berkayyoscimsir2003@hotmail.com>
2020-01-24 13:51:01 +0300

    Sorting by createdAt feeds, comments, blogs of user

--- src/graphql/resolvers/query/User.ts
+++ src/graphql/resolvers/query/User.ts
@@ -5,12 +5,27 @@ import Comment, { IComment } from "../../../models/Comment";
 
 export const User: IQueryType = {
   comments: async (parent): Promise<IComment[]> => {
-    return await Comment.find({ user_id: parent.id });
+    const comments: IComment[] = await Comment.find({ user_id: parent.id });
+    const sortedComments: IComment[] = comments.sort(
+      (a: any, b: any) => Number(a.createdAt) - Number(b.createdAt) > 0 && -1
+    );
+
+    return sortedComments;
   },
   blogs: async (parent): Promise<IBlog[]> => {
-    return await Blog.find({ owner_id: parent.id });
+    const blogs: IBlog[] = await Blog.find({ owner_id: parent.id });
+    const sortedBlogs: IBlog[] = blogs.sort(
+      (a: any, b: any) => Number(a.createdAt) - Number(b.createdAt) > 0 && -1
+    );
+
+    return sortedBlogs;
   },
   feeds: async (parent): Promise<IFeed[]> => {
-    return await Feed.find({ user_id: parent.id });
+    const feeds: IFeed[] = await Feed.find({ user_id: parent.id });
+    const sortedFeeds: IFeed[] = feeds.sort(
+      (a: any, b: any) => Number(a.createdAt) - Number(b.createdAt) > 0 && -1
+    );
+
+    return sortedFeeds;
   }
 };
